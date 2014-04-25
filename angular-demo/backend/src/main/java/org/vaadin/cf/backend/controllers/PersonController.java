a98ac0a6e120006d7fca0f7603da18e2f24a5491
Kim Leppänen <kimleppanen@Kims-MacBook-Pro.local>
2014-04-25 14:48:55 +0300

    Implemented adding of person

--- angular-demo/backend/src/main/java/org/vaadin/cf/backend/controllers/PersonController.java
+++ angular-demo/backend/src/main/java/org/vaadin/cf/backend/controllers/PersonController.java
@@ -3,20 +3,28 @@ package org.vaadin.cf.backend.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.vaadin.cf.backend.data.Person;
 import org.vaadin.cf.backend.repositories.PersonRepository;
 
 @RestController
 public class PersonController {
-	
+
 	@Autowired
 	private PersonRepository personRepository;
-	
-	@RequestMapping("/findAll")
+
+	@RequestMapping("/findAll")	
 	public List<Person> getPersons() {
 		return personRepository.findAll();
+	}
+
+	@RequestMapping(value = "/addPerson", method = RequestMethod.POST)
+	public Person addPerson(@RequestBody Person person) {
+		personRepository.saveAndFlush(person);
+		return person;
 	}
 
 }
